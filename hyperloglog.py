671a4606499d45fbf1e1451d85656be21b0f86b8
Bzykowa <karcia956@gmail.com>
2023-04-18 12:07:36 +0200

    change alpha approximation method

--- hyperloglog.py
+++ hyperloglog.py
@@ -9,7 +9,7 @@ from test_hashes import Hash32Bit
 
 MIN_B = 4
 MAX_B = 16
-N = 10 ** 3
+N = 10 ** 4
 
 # HyperLogLog and helper functions
 
@@ -22,19 +22,31 @@ def ro(s: str) -> int:
             idx = idx + 1
         else:
             break
-    return idx
+    return idx + 1
 
 
 def alpha_m_fu(u, m):
     """Return f(u) value for alpha_m calculations."""
-    return np.power(np.log2(np.divide((2 + u), (1 + u))), m)
+    return np.log2(np.divide((2 + u), (1 + u))) ** m
 
-
-def calculate_alpha_m(m: int):
+def calculate_alpha_m(b: int):
     """Return alpha_m value."""
-    result, _ = quad(alpha_m_fu, 0, np.inf, args=[m])
-    final_result = np.power(result * m, -1)
-    return final_result
+    # integral starts to give wrong results at high m values 
+    # so using alpha from paper for b >= 4
+    if b < 4:
+        m = 2 ** b
+        result, _ = quad(alpha_m_fu, 0, np.inf, args=[m])
+        final_result = (result * m) ** (-1)
+        return final_result
+    elif b == 4:
+        return 0.673
+    elif b == 5:
+        return 0.697
+    elif b == 6:
+        return 0.709
+    elif b >= 7:
+        return 0.7213 / (1 + 1.079 / (2 ** b))
+
 
 
 def hyperLogLog(M_set: Multiset, b: int, h: Hash32Bit) -> float:
@@ -52,9 +64,9 @@ def hyperLogLog(M_set: Multiset, b: int, h: Hash32Bit) -> float:
     """
 
     m = 2 ** b
-    alpha = calculate_alpha_m(m)
+    alpha = calculate_alpha_m(b)
 
-    M = [0 for _ in range(m)]
+    M = [0 for _ in range(m+1)]
     elems = sorted(M_set)
     random.shuffle(elems)
 
@@ -63,32 +75,33 @@ def hyperLogLog(M_set: Multiset, b: int, h: Hash32Bit) -> float:
         # Divide hash to h_1h_2..h_b and h_b+1..h_32
         h_1 = h_e[:b]
         w = h_e[b:]
-        j = int(h_1, 2)
+        j = int(h_1, 2) + 1
 
         M[j] = max(M[j], ro(w))
 
-    E = alpha * np.power(m, 2) * (sum([2 ** (-j) for j in M]) ** (-1))
+    E = alpha * (m ** 2) * (sum([2 ** (-i) for i in M[1:]]) ** (-1))
 
     # Corrections (relative error +- 1.04/sqrt(m) from 2007 paper)
     if E <= 2.5*m:
-        V = len(list(filter(lambda zero: zero == 0, M)))
+        V = len(list(filter(lambda zero: zero == 0, M[1:])))
         if V > 0:
-            return m * np.log(m/V)
+            return m * np.log(m/V), 1
         else:
-            return E
+            return E, 2
     elif E <= (2**32) / 30:
-        return E
+        return E, 2
     else:
-        return -(2**32) * np.log(1 - (E/(2**32)))
+        log = ((2 ** 32) - E)/(2**32)
+        return -(2**32) * np.log(log), 3
 
 # Graphs
 
 
 def graphBs(x_axis: list, y_axis: list, save: bool = False) -> None:
-    plt.scatter(x=x_axis, y=y_axis[0], c='red', label='m=2^4', s=5)
-    plt.scatter(x=x_axis, y=y_axis[1], c='yellow', label='m=2^8', s=5)
-    plt.scatter(x=x_axis, y=y_axis[2], c='green', label='m=2^12', s=5)
-    plt.scatter(x=x_axis, y=y_axis[3], c='cyan', label='m=2^16', s=5)
+    plt.scatter(x=x_axis, y=y_axis[0], c='red', label='m=2^4')
+    plt.scatter(x=x_axis, y=y_axis[1], c='yellow', label='m=2^8')
+    plt.scatter(x=x_axis, y=y_axis[2], c='green', label='m=2^12')
+    plt.scatter(x=x_axis, y=y_axis[3], c='cyan', label='m=2^16')
     plt.xlabel('n')
     plt.ylabel(r'$\frac{\hat{n}}{n}$')
     plt.title(r'$\frac{\hat{n}}{n}$ for different m. (5b)')
@@ -107,17 +120,20 @@ def test_different_b() -> None:
     bs = [4, 8, 12, 16]
     plot_x = [i for i in range(1, N+1)]
     plot_y = [[], [], [], []]
+    ranges = [0, 0, 0]
 
     for i, b in enumerate(bs):
         print(f"b = {b}")
         for n in range(1, N+1):
             end = start + n
-            mset = generateMultiset(start, end, (1, 2))
-            n_hat = hyperLogLog(mset, b, h)
+            mset = generateMultiset(start, end, (1, 1))
+            n_hat, logrange = hyperLogLog(mset, b, h)
+            ranges[logrange-1] += 1
             plot_y[i].append(n_hat/n)
             start = end
 
     graphBs(plot_x, plot_y, True)
+    print(ranges)
 
 
 def test_different_h() -> None:
