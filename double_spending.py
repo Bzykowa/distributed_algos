763dc99c82fdc5bdc7210ceea5ee2feb4397d453
Bzykowa <karcia956@gmail.com>
2023-05-16 17:13:32 +0200

    implement monte carlo simulator plus analysis

--- double_spending.py
+++ double_spending.py
@@ -1,14 +1,21 @@
 from matplotlib import pyplot as plt
-from typing import Callable
+from typing import Callable, List
 import numpy as np
 import argparse
 import math
+import random
 
 # Constants
+# Boundaries of q
 LOWER_BOUND_Q = 0
 UPPER_BOUND_Q = 0.5
+# Test values for experiments
 N_S = [1, 3, 6, 12, 24, 48]
 P_S = [0.001, 0.01, 0.1]
+# Difference in chain length when attacker gives up
+GIVE_UP_DIFF = 50
+# Number of experiments in simulator
+SIM_EXP_NUM = 1000
 
 # P(n,q) approximate formulas
 
@@ -30,11 +37,35 @@ def grunspan(n: int, q: float) -> float:
     )
 
 
+def single_attack(n: int, q: float) -> bool:
+    """A single double spending attack experiment."""
+    attacker = 0
+    others = 0
+
+    while others < n or attacker - others < 0:
+        if random.uniform(0, 1) < q:
+            attacker += 1
+        else:
+            others += 1
+
+        if others - attacker >= GIVE_UP_DIFF:
+            break
+
+    return attacker - others >= 0
+
+
 def double_spending_sim(n: int, q: float) -> float:
     """
     Experimental estimation of P(n,q) depending on n and q with Monte Carlo.
     """
-    pass
+    wins = 0
+
+    for _ in range(SIM_EXP_NUM):
+        if single_attack(n, q):
+            wins += 1
+
+    return wins / SIM_EXP_NUM
+
 
 # Experiments
 
@@ -88,6 +119,72 @@ def n_by_q(p: Callable, samples: int, name: str, save: bool) -> None:
     plt.show()
 
 
+def success_by_q_agg(samples: int, save: bool):
+    """Plot 1st experiment for all approximations."""
+    colors = ["xkcd:neon green", "black", "xkcd:shocking pink",
+              "xkcd:hot purple", "xkcd:sunflower yellow", "xkcd:cerulean blue"]
+    styles = ["solid", "dashed", "dotted"]
+    ps = [double_spending_sim, nakamoto, grunspan]
+    names = ["Monte Carlo", "Nakamoto", "Grunspan"]
+    plt.rcParams["figure.figsize"] = (15, 8)
+
+    q_space = np.linspace(LOWER_BOUND_Q, UPPER_BOUND_Q, samples)
+    for j, p in enumerate(ps):
+        for i, n in enumerate(N_S):
+            plt.plot(
+                q_space, [p(n, q) for q in q_space],
+                label=f"{names[j]}: n = {n}", color=colors[i],
+                linestyle=styles[j]
+            )
+
+    plt.title(r"$P(n, q)$ - all approximations")
+    plt.ylabel(r"$P(n, q)$")
+    plt.xlabel("q")
+    plt.legend()
+
+    if save:
+        plt.savefig("figures/task9c1.png")
+
+    plt.show()
+    plt.rcParams["figure.figsize"] = plt.rcParamsDefault["figure.figsize"]
+
+
+def n_by_q_agg(samples: int, save: bool) -> None:
+    """Plot P(n,q) depending on q for different ns"""
+    colors = ["xkcd:forest green", "gold", "black"]
+
+    q_space = np.linspace(LOWER_BOUND_Q, UPPER_BOUND_Q-0.06, samples)
+    styles = ["solid", "dashed", "dotted"]
+    ps = [double_spending_sim, nakamoto, grunspan]
+    names = ["Monte Carlo", "Nakamoto", "Grunspan"]
+    plt.rcParams["figure.figsize"] = (15, 8)
+
+    for j, p in enumerate(ps):
+        for i, pbb in enumerate(P_S):
+            n_space = []
+            n = 1
+            for q in q_space:
+                while p(n, q) > pbb:
+                    n = n + 1
+                n_space.append(n)
+
+            plt.plot(
+                q_space, n_space, label=f"{names[j]}: p = {pbb}",
+                color=colors[i], linestyle=styles[j]
+            )
+
+    plt.title(r"$n$ meeting $P(n, q)$ for $q$ - all approximations")
+    plt.ylabel("n")
+    plt.xlabel("q")
+    plt.legend()
+
+    if save:
+        plt.savefig("figures/task9c2.png")
+
+    plt.show()
+    plt.rcParams["figure.figsize"] = plt.rcParamsDefault["figure.figsize"]
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Double Spending Analysis")
@@ -101,11 +198,11 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--n", default=1, type=int,
-        help="n parameter for double spending simulator. Default: 1 \n"
+        help="n - number of confirmations needed. Default: 1 \n"
     )
     parser.add_argument(
         "--q", default=0.25, type=float,
-        help="q parameter for double spending simulator. Default: 0.25 \n"
+        help="q - computational power of an attacker (0,1). Default: 0.25 \n"
     )
     args = parser.parse_args()
 
@@ -116,7 +213,13 @@ if __name__ == "__main__":
         n_by_q(nakamoto, 100, "Nakamoto", True)
         n_by_q(grunspan, 100, "Grunspan", True)
     elif args.exp == 3:
-        pass
+        success_by_q_agg(100, True)
+        n_by_q_agg(100, True)
     else:
         # run the simulator for inputted n and q
-        pass
+        pnq = double_spending_sim(args.n, args.q)
+        print(
+            "Probability that attacker outpaces others " +
+            "when/after they have n confirmations."
+        )
+        print(f"n = {args.n}", f"q = {args.q}", f"P(n,q) = {pnq}", sep="\n")
